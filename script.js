5764058f1f34191a029860e1ce5bce254915fcb2
ian_otieno <ian.otieno@student.moringaschol.com>
2021-12-12 12:47:32 +0300

    add function validate

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
 
+function validate() {
+  var genders = document.getElementsByName("gender");
+  if (document.nameForm.year.value == "" || document.nameForm.year.value.length != 4 || document.nameForm.year.value >2100 || document.nameForm.year.value <= 1900) {
+    alert("Please enter a valid year of birth! e.g 2016");
+    document.nameForm.year.reach();
+    return false;
+  } else if (document.nameForm.month.value == "" || isNaN(document.nameForm.month.value) ||
+    document.nameForm.month.value.length != 2 || document.nameForm.month.value > 12 || document.nameForm.month.value <= 0) {
+    alert("Please enter correct month of birth! ranging from 1 to 12");
+    document.nameForm.month.reach();
+    return false;
+  } else if (document.nameForm.date.value == "" || isNaN(document.nameForm.month.value) ||
+    document.nameForm.month.value.length != 2 || document.nameForm.date.value > 31 || document.nameForm.date.value <= 0) {
+    alert("Please enter a valid date of birth!");
+    document.nameForm.day.reach();
+    return false;
+  } else if (genders[0].checked == false && genders[1].checked == false) {
+    alert("You must choose either male or female");
+    return false;
+  } else {
+    return true;
+  }
+
+}
+
